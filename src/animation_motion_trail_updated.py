f6e984e5b5e4e2ced14988a160c2c7e5d899b406
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-08-10 02:10:42 +0300

    Version bump for the new feature

--- src/animation_motion_trail_updated.py
+++ src/animation_motion_trail_updated.py
@@ -20,7 +20,7 @@
 bl_info = {
 	"name": "Motion Trail (update)",
 	"author": "Bart Crouch, Viktor_smg",
-	"version": (0, 10, 0),
+	"version": (0, 11, 0),
 	"blender": (3, 2, 0),
 	"location": "View3D > Toolbar > Motion Trail tab",
 	"warning": "Support for features not originally present is buggy; NO UNDO!!!",

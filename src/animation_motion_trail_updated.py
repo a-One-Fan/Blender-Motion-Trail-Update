815be36a7abc72150ab8785c17bcbc32aa1fd3d3
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-08-10 02:08:48 +0300

    Remove soft_min
    For more handle direction customization

--- src/animation_motion_trail_updated.py
+++ src/animation_motion_trail_updated.py
@@ -2078,7 +2078,6 @@ class MotionTrailProps(bpy.types.PropertyGroup):
 	handle_length: FloatProperty(name="Handle length",
 			description="Handle length multiplier",
 			default = 1.0,
-			soft_min = 0.1,
 			step = 0.15,
 			update=internal_update
 			)
